25dd1e253c89b289fa025cc84a87598291507006
Sergey <spavelin@inbox.ru>
2026-01-07 20:32:07 +0500

    add async handler

--- src/main/kotlin/ru/shaxowskiy/notificationservice/dto/SubscribeInfoDto.kt
+++ src/main/kotlin/ru/shaxowskiy/notificationservice/dto/SubscribeInfoDto.kt
@@ -1,7 +1,4 @@
 package ru.shaxowskiy.notificationservice.dto
 
-import java.time.LocalDateTime
-import java.util.*
-
 class SubscribeInfoDto(val telegram_chat_id: String, val username: String) {
 }
--- src/main/kotlin/ru/shaxowskiy/notificationservice/listeners/PostEventListener.kt
+++ src/main/kotlin/ru/shaxowskiy/notificationservice/listeners/PostEventListener.kt
@@ -2,21 +2,38 @@ package ru.shaxowskiy.notificationservice.listeners
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
 import ru.shaxowskiy.notificationservice.dto.PostEventDto
 import ru.shaxowskiy.notificationservice.repository.UserRepository
 import ru.shaxowskiy.notificationservice.service.TelegramBotService
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 @Component
-class PostEventListener(@Autowired var userRepository: UserRepository,
-                        var telegramBotService: TelegramBotService) {
+class PostEventListener(
+    @Autowired var userRepository: UserRepository,
+    var telegramBotService: TelegramBotService
+) {
+
+    val workerPool: ExecutorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())
 
     @KafkaListener(topics = ["PUBLISHPOSTEVENT"], groupId = "notification-group")
-    fun listen(message: PostEventDto){
-        //TODO Multithreading handle events
-        println("Получено событие ${message.author_post}")
+    fun listen(message: PostEventDto) {
+        val task: Runnable = Runnable {
+            handleMessage(message) //FIXME ack successfully get kafka event (mb user inner support using VT)
+        }
+        workerPool.submit(task)
+    }
+
+    private fun handleMessage(message: PostEventDto) {
         val findSubscribersByUsername = userRepository.findSubscribersByUsername(message.author_post)
 
-        findSubscribersByUsername.forEach{ subscriberInfo -> telegramBotService.sendNotificationMessage(subscriberInfo.telegram_chat_id, subscriberInfo.username)}
+        findSubscribersByUsername.forEach { subscriberInfo ->
+            telegramBotService.sendNotificationMessage(
+                subscriberInfo.telegram_chat_id,
+                subscriberInfo.username
+            )
+        }
     }
 }
